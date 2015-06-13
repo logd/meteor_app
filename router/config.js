a017565ce8897b3e9be32ce7f95fd9e370ef5032
Anders Ramsay <andersr@gmail.com>
2015-06-13 17:50:59 -0400

    social logins (g,fb,tw,ghub)

---
+++ router/config.js
@@ -0,0 +1,25 @@
+Router.configure({
+  layoutTemplate: 'ApplicationLayout',
+   loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound'
+});
+
+
+
+// // GLOBAL
+// // Default settings for all routes
+// Router.configure({
+//   layoutTemplate: 'ApplicationLayout',
+//   loadingTemplate: 'loading',
+//   notFoundTemplate: 'notFound'
+//   // ,
+//   // waitOn: function () {
+//   //   return Meteor.subscribe("userData");
+//   // }
+//   // ,
+//   // waitOn: function() {
+
+//   //   // TODO: this subscribes to posts for every route.  limit what is subscribed to in the future
+//   //   return Meteor.subscribe('posts');
+//   // }
+// });
---
+++ router/routes.js
@@ -0,0 +1,46 @@
+// Router.onBeforeAction(function () {
+//   // all properties available in the route function
+//   // are also available here such as this.params
+
+//   if (!Meteor.userId()) {
+//     // if the user is not logged in, render the Login template
+//     this.render('login');
+//   } else {
+//     // otherwise don't hold up the rest of hooks or our route/action function
+//     // from running
+//     this.next();
+//   }
+// });
+
+// HOMEPAGE
+// rename route to homepage?
+Router.route('/', function (){
+    this.render('home');
+});
+
+// LOGIN
+// Will this be needed
+Router.route('/login', function(){
+   this.render('login');
+});
+
+
+// NEW POST
+// Use Random.id([6]) while found in user ids
+Router.route('/:token_id/new', { name: 'newPost'});
+
+
+// POST DETAIL
+Router.route('/:token_id/:post_title', { name: 'postDetail'});
+
+// EDIT POST
+Router.route('/:token_id/:post_title/edit', { name: 'editPost'});
+
+// POSTS LIST
+Router.route('/:token_id/posts/', { name: 'posts'});
+
+// POSTS LIST - FILTERED
+Router.route('/:token_id/posts/sx-wizard', { name: 'postsListFiltered'});
+
+
+
