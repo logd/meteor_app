6f923e719ba62d1be23036d23ffc032cc755ccbc
Anders Ramsay <andersr@gmail.com>
2015-06-18 11:48:56 -0400

    removed sui

--- client/views/body.js
+++ client/views/body.js
@@ -1,18 +1,5 @@
-  Template.body.onRendered(function(){
-    // this.$('#app-menu').sidebar('attach events', '.launch.button');
-
-    // this.$('.ui.sidebar').sidebar('attach events', '.launch.button');
-    // this.$(".dropdown").dropdown();
-    // this.$('#sidebar').sidebar('attach events', '#sidebar-button');
-
-  });
-
-
-
-  Template.body.events({
-    // "click .sidebar-toggle": function(e,t){
-    //   e.preventDefault();
-    //   t.$("#wrapper").toggleClass("toggled");
-    // }
-  });
+  // Template.body.onRendered(function(){
+  // });
+  // Template.body.events({
+  // });
 
--- client/views/layouts/app_layout.js
+++ client/views/layouts/app_layout.js
@@ -1,42 +1,6 @@
-Template.AppLayout.onRendered(function(){
-
-    // this.$('#foo').sidebar('toggle');
-
-    // this.$('.sidebar').sidebar('toggle');
-
-    // SEMANTIC UI Components
-    // this.$(".dropdown").dropdown();
-
-    // this.$('#sidebar').sidebar('attach events', '#sidebar-button');
-
-  });
-
 Template.AppLayout.events({
-    "click .sidebar-toggle": function(e,t){
-      e.preventDefault();
-      t.$(".app-container").toggleClass("toggled");
-    }
-
-    // "click .ui.sidebar": function(e,t){
-    //   e.preventDefault();
-    //   t.sidebar('attach events', '.launch.button');
-    // }
-  });
-
-
-  // Template.body.onRendered(function(){
-
-
-    // this.$('.sidebar').sidebar('toggle');
-    // this.$('#app-menu-button').sidebar('toggle');
-    
-    // this.$('.left.demo.sidebar').first().sidebar('attach events', '.toggle.button');
-
-    // this.$('.toggle.button').removeClass('disabled');
-    // this.$('#app-menu').sidebar('toggle');
-
- 
-
-    // this.$(".dropdown").dropdown();
-
-  // });
+  "click .sidebar-toggle": function(e,t){
+  e.preventDefault();
+  t.$(".app-container").toggleClass("toggled");
+  }
+});
