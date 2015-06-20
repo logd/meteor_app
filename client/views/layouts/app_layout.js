8c48c5247136630d55e5dce238e098e64f2cc9c0
Anders Ramsay <andersr@gmail.com>
2015-06-20 14:44:25 -0400

    mobile sidebar working

--- client/views/layouts/app_layout.js
+++ client/views/layouts/app_layout.js
@@ -1,5 +1,5 @@
 Template.AppLayout.events({
-  "click .sidebar-toggle": function(e,t){
+  "click .app-nav-toggle": function(e,t){
   e.preventDefault();
   t.$(".app-container").toggleClass("toggled");
   }
