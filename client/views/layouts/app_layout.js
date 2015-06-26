d850c629b8b89cb3b3a0a3e758de61a5aa88adc9
Anders Ramsay <andersr@gmail.com>
2015-06-26 08:44:38 -0400

    edit view in progress

--- client/views/layouts/app_layout.js
+++ client/views/layouts/app_layout.js
@@ -22,32 +22,7 @@ Template.AppLayout.events({
   t.$(".app-container").toggleClass("toggled");
   },
   "click .done-editing" : function(e,t){
-    // var postContent = $("#postContent").val();
-    // var hashtagPattern = /#[A-Za-z0-9_]*/gi;
-
-    // function removeFirstChar(str){
-    //   return str.substr(1, str.length);
-    // };
-    // var rawTags = tags = [];
-    // var postTags = _.map(rawTags, function(tag) {
-    //       return removeFirstChar(tag);
-    //     });
-    // var contentLines = postContent.split("\n");
-    // var postTitle = ( contentLines[0].length > 50  ?  contentLines[0].substring(0,50) : contentLines[0] );
     
-    // var post = {
-    //   title: postTitle, 
-    //   content: postContent,
-    //   tags: postTags
-    // };
-
-    // Meteor.call('postInsert', post, function(error, result){
-    //     if(error) {return alert(error.reason)};
-    
-    //     console.log(result._id);
-    //     Router.go('show_post', {_id: result._id });     
-  
-    // });
 
     Router.go('show_post', {_id: Router.current().params._id });
   },
--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -3,64 +3,54 @@
 // });
 
 Template.edit_post.helpers({
-  // editPost: function() {
-  //   return Posts.findOne(this.params._id);
-  // },
-  // postIsEmpty: function() {
-  //   if(postContent.trim() === null || postContent.trim() === ""){
-  //       Session.set("postHasContent", false);
-  //       return true;
-
-  //     } else {
-  //       Session.set("postHasContent", true);
-  //       return false;
-  //     };
-  // }
 });
 
 Template.edit_post.events({
-  "input .post-form" : function(e, t){
-    var postContent = $('#postContent').val();
-    Session.set('save-notice', "Saving changes...");
+  // "input .post-form" : function(e, t){
+  //   var postContent = $('#postContent').val();
 
-    var autoSaveInterval = function(){
-      var timer;
+  //   // on input, every 3 seconds, save changes
 
-      this.set = function(saveForm) {
-        timer = Meteor.setTimeout(function() {
-          // saveFormCB();
-          Meteor.call(
-            'updatePost',
-            postAttributes,
-            function(error, result){
-              if (error){ alert(error.reason);}
-            });
-          }, 3000)
-      };
+  //   // Session.set('save-notice', "Saving changes...");
 
-      this.clear = function() {
-        Meteor.clearInterval(timer);
-      };
+  //   var autoSaveInterval = function(){
+  //     var timer;
 
-        return this;    
-    }();
+  //     this.set = function(saveForm) {
+  //       timer = Meteor.setTimeout(function() {
+  //         // saveFormCB();
+  //         Meteor.call(
+  //           'updatePost',
+  //           postAttributes,
+  //           function(error, result){
+  //             if (error){ alert(error.reason);}
+  //           });
+  //         }, 3000)
+  //     };
 
-     // Save user input after 3 seconds of not typing
-    autoSaveInterval.clear()
+  //     this.clear = function() {
+  //       Meteor.clearInterval(timer);
+  //     };
 
-    autoSaveInterval.set(function() {   
-      // We should update our document. 
-      // If update is successful, then
-      // Session.set('saving', 'All Changes Saved.'); 
-   });
+  //       return this;    
+  //   }();
+
+  //    // Save user input after 3 seconds of not typing
+  //   autoSaveInterval.clear()
+
+  //   autoSaveInterval.set(function() {   
+  //     // We should update our document. 
+  //     // If update is successful, then
+  //     // Session.set('saving', 'All Changes Saved.'); 
+  //  });
 
 
-    //
-    // edit icon should switch to done icon
-    // want to support all the same functionality as for new post here, except the Meteor call will be to upsert
-    // do I want to explore
+  //   //
+  //   // edit icon should switch to done icon
+  //   // want to support all the same functionality as for new post here, except the Meteor call will be to upsert
+  //   // do I want to explore
 
-  }
+  // }
 });
  
 
