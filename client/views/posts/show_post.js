e51b62b7c4394edca83c64e29ca4cea184a94584
Anders Ramsay <andersr@gmail.com>
2015-07-01 17:51:53 -0400

    add truncated title to Logd

--- client/views/posts/show_post.js
+++ client/views/posts/show_post.js
@@ -1,9 +1,5 @@
 Template.show_post.helpers({
   truncatedTitle: function() {
-    if(this.title.length > 20) {
-      return this.title.substr(0, 19);
-    } else {
-      return false;
-    };
+    return Logd.posts.truncateTitle(this.title);
   }
 });
