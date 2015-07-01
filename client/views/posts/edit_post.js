67ef4ac0998b6b236abe50b1c8e5fdcc1cbd1c6f
Anders Ramsay <andersr@gmail.com>
2015-07-01 16:39:22 -0400

    refactor post edit to use blur

--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -13,82 +13,58 @@ Template.edit_post.helpers({
 });
 
 Template.edit_post.events({
-  "blur .post-content" : function(e,t){
-    var postContent = $('.post-content').val();
-    console.log(postContent);
+  "click .edit-post-title": function(){
+    Session.set('editingPostTitle', true);
+    this.$('.post-title').focus();
   },
-  "click .edit-post-title": function(e,t){
-    e.preventDefault();
+  "click .edit-post-content": function(){
+    Session.set('editingPostContent', true);
+    this.$('.post-content').focus();
+  },
+  "blur .post-title" : function(e,t){
+    var postContent = $('.post-title').val();
+    console.log(postContent);
 
-    // if editing post content, then update that first
-    if (Session.get('editingPostContent')){
-      var postContent = $('.post-content').val();
-      var postTags = Logd.tags.getHashTags(postContent);
+    var postTags = Logd.tags.getHashTags(postContent);
 
-      var postAttributes = {
-        postId: Router.current().params._id,
-        content: postContent,
-        tags: postTags
-      };
-
-      Meteor.call('updatePostContent', postAttributes, function(error, result){
-        if (error){
-          alert(error.reason);
-        };
-      });
+    var postAttributes = {
+      postId: Router.current().params._id,
+      title: postContent,
+      tags: postTags
     };
+
+    Meteor.call('updatePostTitle', postAttributes, function(error, result){
+      if (error){
+        alert(error.reason);
+      };
+    });
+
+    Session.set('editingPostTitle', false);
+
+  },
+  "blur .post-content" : function(e,t){
+  var postContent = $('.post-content').val();
+  console.log(postContent);
+
+  var postTags = Logd.tags.getHashTags(postContent);
+
+  var postAttributes = {
+    postId: Router.current().params._id,
+    content: postContent,
+    tags: postTags
+  };
+
+  Meteor.call('updatePostContent', postAttributes, function(error, result){
+    if (error){
+      alert(error.reason);
+    };
+  });
 
     Session.set('editingPostContent', false);
-    Session.set('editingPostTitle', true);
-  },
-    "click .edit-post-content": function(e,t){
-    e.preventDefault();
 
-    // if editing post title, then update that first
-    if (Session.get('editingPostTitle')){
-      var postTitle = $('.post-title').val();
-      console.log(postTitle);
-      var postTags = Logd.tags.getHashTags(postTitle);
-    
-      var postAttributes = {
-        postId: Router.current().params._id,
-        title: postTitle,
-        tags: postTags
-      };
-
-      Meteor.call('updatePostTitle', postAttributes, function(error, result){
-        if (error){
-          alert(error.reason);
-        };
-
-      }); 
-    };
-     Session.set('editingPostTitle', false);
-     Session.set('editingPostContent', true);
-  
   },
   "click .done-editing": function(e,t){
     e.preventDefault();
-    var postTitle = $('.post-title').val() || this.title;
-    var postContent = $('.post-content').val() || this.content || "";
-   
-    var postTitleTags = Logd.tags.getHashTags(postTitle);
-    var postContentTags = Logd.tags.getHashTags(postContent);
-    var postTags = postTitleTags.concat(postContentTags);
-
-      var postAttributes = {
-        postId: Router.current().params._id,
-        title: postTitle,
-        content: postContent,
-        tags: postTags 
-      };
-
-      Meteor.call('updatePost', postAttributes, function(error, result){
-        if (error){
-          alert(error.reason);
-        } else {
-          Router.go('show_post', { _id: Router.current().params._id });
-        };
-      });  
+    Router.go('show_post', { _id: Router.current().params._id }); 
   }
 });
