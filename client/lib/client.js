718ae266ebdc95f243ef1edbbf397bad653cb2bf
Anders Ramsay <andersr@gmail.com>
2015-07-02 13:37:11 -0400

    avatars displaying

--- client/lib/client.js
+++ client/lib/client.js
@@ -17,27 +17,31 @@
 
 Meteor.subscribe("posts");
 Meteor.subscribe("tags");
+Meteor.subscribe("userData");
+
+// Accounts.ui.config({ passwordSignupFields: 'USERNAME_AND_EMAIL' });
 
  Template.body.helpers({
-    users: function () {
-      return Meteor.users.find().fetch();
-    },
-    service: function (user) {
-      if      (user && user.services && user.services.twitter)   { return 'twitter'; }
-      else if (user && user.services && user.services.facebook)  { return 'facebook'; }
-      else if (user && user.services && user.services.google)    { return 'google'; }
-      else if (user && user.services && user.services.github)    { return 'github'; }
-      else if (user && user.services && user.services.instagram) { return 'instagram'; }
-      else                                                       { return 'none'; }
-    },
-    name: function (user) {
-      var name = '';
-      if (user && user.profile && user.profile.name) {
-        name = user.profile.name;
-      }
-      else if (user && user.username) {
-        name = user.username;
-      }
-      return name;
+  current_user: function(){
+      return Meteor.user();
     }
+    // users: function () {
+    //   return Meteor.users.find().fetch();
+    // },
+    // service: function (user) {
+    //  if (user && user.services && user.services.google)    { return 'google'; }
+    //   else if (user && user.services && user.services.github)    { return 'github'; }
+    //   else if (user && user.services && user.services.instagram) { return 'instagram'; }
+    //   else                                                       { return 'none'; }
+    // },
+    // name: function (user) {
+    //   var name = '';
+    //   if (user && user.profile && user.profile.name) {
+    //     name = user.profile.name;
+    //   }
+    //   else if (user && user.username) {
+    //     name = user.username;
+    //   }
+    //   return name;
+    // }
   });
--- server/server.js
+++ server/server.js
@@ -8,6 +8,20 @@ Meteor.publish("posts", function(){
   return Posts.find();
 });
 
+
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find( {_id: this.userId},
+      {fields: 
+        {
+          "services.google.picture": true
+        }
+      });
+  } else {
+    this.ready();
+  }
+});
+
 // Meteor.publish("tags", function(){
 //   return Tags.find();
 // });
