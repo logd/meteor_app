b5e8050831696d5e4413751631ad15c018fe6499
Anders Ramsay <andersr@gmail.com>
2015-07-02 14:19:20 -0400

    new post view

--- server/server.js
+++ server/server.js
@@ -1,9 +1,3 @@
- Meteor.startup(function () {
-    console.log('startup...');
-    console.log('options:', Avatar.options);
-    console.log('url:', Avatar.getUrl());
-  });
-
 Meteor.publish("posts", function(){
   return Posts.find();
 });
