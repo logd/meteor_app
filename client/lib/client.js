d8d9cf68c4eb5485a750db11d3a2d16b8b187842
Anders Ramsay <andersr@gmail.com>
2015-07-02 11:02:54 -0400

    display avatar debug

--- client/lib/client.js
+++ client/lib/client.js
@@ -17,3 +17,27 @@
 
 Meteor.subscribe("posts");
 Meteor.subscribe("tags");
+
+ Template.body.helpers({
+    users: function () {
+      return Meteor.users.find().fetch();
+    },
+    service: function (user) {
+      if      (user && user.services && user.services.twitter)   { return 'twitter'; }
+      else if (user && user.services && user.services.facebook)  { return 'facebook'; }
+      else if (user && user.services && user.services.google)    { return 'google'; }
+      else if (user && user.services && user.services.github)    { return 'github'; }
+      else if (user && user.services && user.services.instagram) { return 'instagram'; }
+      else                                                       { return 'none'; }
+    },
+    name: function (user) {
+      var name = '';
+      if (user && user.profile && user.profile.name) {
+        name = user.profile.name;
+      }
+      else if (user && user.username) {
+        name = user.username;
+      }
+      return name;
+    }
+  });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+ Meteor.startup(function () {
+    console.log('startup...');
+    console.log('options:', Avatar.options);
+    console.log('url:', Avatar.getUrl());
+  });
+
 Meteor.publish("posts", function(){
   return Posts.find();
 });
