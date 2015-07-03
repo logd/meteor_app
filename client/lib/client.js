61adca3b98e1a8db508ae4eaa05964b076006a44
Anders Ramsay <andersr@gmail.com>
2015-07-03 09:35:39 -0400

    dropdown test

--- client/lib/client.js
+++ client/lib/client.js
@@ -21,6 +21,33 @@ Meteor.subscribe("userData");
 
 // Accounts.ui.config({ passwordSignupFields: 'USERNAME_AND_EMAIL' });
 
+Template.body.onRendered(function(){
+
+
+// this.$('.sidebar').sidebar('toggle');
+// this.$('#app-menu-button').sidebar('toggle');
+
+// this.$('.left.demo.sidebar').first().sidebar('attach events', '.toggle.button');
+
+// this.$('.toggle.button').removeClass('disabled');
+// this.$('#app-menu').sidebar('toggle');
+
+// this.$('.ui.sidebar')
+//   .sidebar('attach events', '.launch.button')
+// ;
+
+// this.$(".dropdown").dropdown();
+
+});
+
+Template.home.onRendered(function(){
+
+// SEMANTIC UI Components
+this.$(".dropdown").dropdown();
+
+});
+
+
  Template.body.helpers({
   current_user: function(){
       return Meteor.user();
