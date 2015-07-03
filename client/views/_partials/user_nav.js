8a8ba3bd1d6b552403adaa0086291edfb334f95b
Anders Ramsay <andersr@gmail.com>
2015-07-03 11:29:14 -0400

    custom logout

--- client/views/_partials/user_nav.js
+++ client/views/_partials/user_nav.js
@@ -1,9 +1,24 @@
 Template.user_nav.helpers({
   user_name: function () {
-    if(Meteor.user().services.google){
+    // TODO: this creates a bug, get the name a different way
+    if(Meteor.user()){
       return Meteor.user().services.google.name;
     } else {
       return "Current User";
     }
   }
+});
+
+Template.user_nav.events({
+  "submit .logout_event": function (e,t) {
+    e.preventDefault();
+    Meteor.logout(function(error){
+      if(error){
+        alert(error);
+      }
+       Router.go('login');
+    });
+   
+  }
+
 });
