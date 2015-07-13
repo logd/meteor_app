5b63b31b0ddf0719e76c95aeb56763f935dc58a1
Anders Ramsay <andersr@gmail.com>
2015-07-13 14:29:37 -0400

    recent tags links

--- client/views/tags/recent_tags.js
+++ client/views/tags/recent_tags.js
@@ -3,3 +3,16 @@ Template.recent_tags.helpers({
     return Tags.find({"authors.authorId": Meteor.userId()}, {sort: {"authors.lastUsed": -1}, limit: 3}).fetch();
   }
 });
+
+Template.recent_tags.events({
+"click .tag-matches": function(e) {
+      //TODO: refactor: this is the same that is used for tags_list
+      e.preventDefault();
+      var selected_tag =  $(e.target).text();
+      Router.go('tag_matches', { tag: selected_tag });          
+  },
+    "click .tags-list": function(event) {
+    event.preventDefault();
+    Router.go('tags_list');
+  }
+});
