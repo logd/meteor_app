b4c56576fecefddc54eecd0aa29af3eb68683026
Anders Ramsay <andersr@gmail.com>
2015-07-13 16:55:20 -0400

    search in progress

--- client/lib/client.js
+++ client/lib/client.js
@@ -8,18 +8,45 @@ Meteor.subscribe("posts");
 Meteor.subscribe("tags");
 Meteor.subscribe("userData");
 
+UI.registerHelper('appHeaderCenter', function() {
+  var current_view = Router.current().route.getName();
 
-Template.body.onRendered(function(){
+  switch (current_view){
+      case 'login':
+        return "Logd";
+
+      case 'tag_matches':
+        return "#" + Router.current().params.tag;
+
+      case 'edit_post':
+        return this.title;
+
+      case 'show_post':
+        return this.title;
+
+      case 'tags_list':
+        return "My Topics";
+
+      case 'search':
+        return false;
+
+      default: 
+        return "Logd";
+    };
 
 });
 
-Template.home.onRendered(function(){
+// Template.body.onRendered(function(){
 
-});
+// });
+
+// Template.home.onRendered(function(){
+
+// });
 
 
- Template.body.helpers({
-  // current_user: function(){
-  //     return Meteor.user();
-  //   }
-  });
+ // Template.body.helpers({
+ //  // current_user: function(){
+ //  //     return Meteor.user();
+ //  //   }
+ //  });
