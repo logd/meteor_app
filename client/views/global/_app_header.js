75bbc817b53d6029079a4e325ad3fabf0d16e0c3
Anders Ramsay <andersr@gmail.com>
2015-07-13 12:08:07 -0400

    only return shortenedTitle for edit and show post

--- client/views/global/_app_header.js
+++ client/views/global/_app_header.js
@@ -32,7 +32,13 @@ Template.app_header.helpers({
     };
   },
   shortenedTitle: function() {
-    return Logd.posts.shortenedTitle(this.title);
+    var current_view = Router.current().route.getName();
+    if(current_view === 'edit_post' || current_view === 'show_post' ){
+      return Logd.posts.shortenedTitle(this.title);
+    } else {
+      return false;
+    }
+    
   },
     saveNotice: function() {
     if(Session.get("hasContent")){
