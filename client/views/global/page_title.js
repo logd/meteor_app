d83b428b5e4675f44c3cf6f31557e9690508ef2b
Anders Ramsay <andersr@gmail.com>
2015-07-18 16:25:43 -0400

    reactive post title in show view

--- client/views/global/page_title.js
+++ client/views/global/page_title.js
@@ -11,36 +11,12 @@ Template.page_title.helpers({
         return "#" + Router.current().params.tag;
 
       case 'edit_post':
-        var postId = Router.current().params. _id;
-        var postTitleId = "postTitle-" + postId;
-        var thisPostTitle = Session.get(postTitleId);
-
-        // BUG: TITLE VALUE BEING PASSED HERE IS 'UNDEFINED'
-        // 1. get content from post-content?, 2. if this.title is empty, return 'Untitled post', 3. else return shortenedTitle
-        // var content = Template.instance().edit_post.find('.post-form textarea').value;
-        // console.log("edit post title: " + this.title);
-        // var title = Logd.posts.getTitle(this.title);
-        // return Logd.posts.shortenedTitle(title);
-        // var post = Posts.findOne({_id: Router.current().params._id});
-        // var title = post.title;
-        // // var content = Session.get("postContent");
-        // console.log("Edit post title: " + title);
-        // // return Logd.posts.shortenedTitle(title);
-
-        // if(Logd.posts.hasContent(title)){
-        //   return Logd.posts.shortenedTitle(title);
-        // } else {
-        //   return "Edit Post";
-        // }
-
+        var thisPostTitle = Logd.posts.getPostTitle(Router.current().params._id);
         return Logd.posts.shortenedTitle(thisPostTitle);
     
       case 'show_post':
-
-        // BUG: How do I get the title value here?
-        //I am passing it in the show_post controller data context so it seems this.title should work
-        console.log("show title: " + this.title);
-         return Logd.posts.shortenedTitle(this.title);
+        var thisPostTitle = Logd.posts.getPostTitle(Router.current().params._id);
+        return Logd.posts.shortenedTitle(thisPostTitle);
 
       case 'tags_list':
         return "My Topics";
