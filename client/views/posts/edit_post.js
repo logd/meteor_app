d156a2cc47445afae26d8470707ae837217c11e8
Anders Ramsay <andersr@gmail.com>
2015-07-24 15:17:00 -0400

    save refactor in progress

--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -16,32 +16,32 @@ Template.edit_post.helpers({
 Template.edit_post.events({
   "keyup .has-content": function(event, template){ 
      var content = event.target.value;
-     var contentTest = template.content;
-     console.log("post content: " + Session.get("postContent"));
+     // var contentTest = template.content;
+     // console.log("post content: " + Session.get("postContent"));
 
 
-     Logd.posts.hasContent(content) ?
-        Session.set('hasContent', true) :
-        Session.set('hasContent', false);
+     // Logd.posts.hasContent(content) ?
+     //    Session.set('hasContent', true) :
+     //    Session.set('hasContent', false);
   },
   "input .editor textarea": function(event,template){
     //On create, content is empty...
-    var content = event.target.value;
-    var postId = Router.current().params._id;
+    // var content = event.target.value;
+    // var postId = Router.current().params._id;
 
     // set up autoSaving of post data, updating displayed post title, and adding any new tags to db
     var autoSave = function() {
 
       // this actually saves to the db and returns a post object
-      var post = Logd.posts.saveChanges(content, postId);
+      // var post = Logd.posts.saveChanges(content, postId);
 
       // reactively updated the displayed post title
-      Logd.posts.setPostTitle(postId, post.title);
+      // Logd.posts.setPostTitle(postId, post.title);
 
-      // if tags were found, add new ones to db
-      if (post.tags.length > 0) {
-         LogdTags.upsertTags(post.tags);
-      };
+      // // if tags were found, add new ones to db
+      // if (post.tags.length > 0) {
+      //    LogdTags.upsertTags(post.tags);
+      // };
     };
 
     // on input, if post content has content,
