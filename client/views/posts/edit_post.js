e691ed3e2c1f53f94dfd514fd2cdb5288442421a
Anders Ramsay <andersr@gmail.com>
2015-07-26 15:09:12 -0400

    always save after 1.5s

--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -1,3 +1,22 @@
+
+Template.edit_post.onCreated(function(){
+
+  // get the current template instance
+  // var instance = this;
+ 
+
+  // subscribe to content publication
+
+  // set a local instance variable for the current content
+
+  // compare them every 1.5s
+
+  // if they are different, save changes
+
+  
+});
+
+
 Template.edit_post.onRendered(function(){
  $('.auto-size').autosize();
 });
@@ -14,24 +33,42 @@ Template.edit_post.onRendered(function(){
 // });
 
 Template.edit_post.events({
-  "input .editor textarea": function(event,template){
+  "input .editor textarea": function(event,instance){
+     var postAttributes = {
+        postId: Router.current().params._id,
+        content: event.target.value
+      };
 
-    var postContent = event.target.value;
 
-    var postAttributes = {
-      postId: Router.current().params._id,
-      content: postContent,
-      tags: LogdTags.getTags(postContent)
-    };
+    // on input, start 1.5s timer
+    Meteor.setTimeout(function() {
+
+      // after 1.5s, get current content
+      postAttributes.content = event.target.value;
+      postAttributes.tags = LogdTags.getTags(postAttributes.content);
    
-    if(LogdPosts.hasContent(postContent)){
-      Session.set("hasContent", true);
-      LogdPosts.autoSaveTimer.resetTimer();
-      LogdPosts.autoSaveTimer.startTimer(postAttributes);
-    } else {
-      Session.set("hasContent", false);
-    };
+       // if content is not empty      
+      if(LogdPosts.hasContent(postAttributes.content)){
 
+         // inform about this globally (not good, but oh well)
+         Session.set("hasContent", true);
+
+         // reset the "pause typing" timer
+         LogdPosts.saveOnPauseTimer.resetTimer();
+
+          // update changes
+         LogdPosts.updatePost(postAttributes);
+
+      } else {
+        Session.set("hasContent", false);
+      };
+    }, 1500);
+
+    postAttributes.content = event.target.value;
+    postAttributes.tags = LogdTags.getTags(postAttributes.content);
+    LogdPosts.saveOnPauseTimer.resetTimer(); 
+    LogdPosts.saveOnPauseTimer.startTimer(postAttributes);
+    
   },
   "blur .editor textarea": function(event){
     var postContent = event.target.value;
